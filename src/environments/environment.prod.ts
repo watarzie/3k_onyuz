7559465fb6e4fd18d75bc88607c05a31ad94157d
watarzie <omer-akkaya@outlook.com>
2026-05-21 00:43:12 +0300

    prod settings

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiBaseUrl: '/api'
+  apiBaseUrl: 'https://www.3klojistikdepolama.com/api'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  production: false,
-  apiBaseUrl: 'http://localhost:50447/api'
+  production: true,
+  apiBaseUrl: 'https://www.3klojistikdepolama.com/api'
 };
